6f176b98919b77158222fceb8a28d02129e05347
Sonia Uduma <sohnyauduma@gmail.com>
2021-07-09 17:53:23 +0100

    fixed onload page ui

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
-const form = document.querySelector("#show-search");
-const inputField = document.querySelector("#search-field");
-const resultContainer = document.querySelector(".result-container");
-const searchBtn = document.querySelector(".search-btn");
+// const form = document.querySelector("#show-search");
+// const inputField = document.querySelector("#search-field");
+// const resultContainer = document.querySelector(".search-container");
+// const searchBtn = document.querySelector(".search-btn");
+// const onloadContainer = document.querySelector(".onload-container");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  getData();
-  inputField.value = "";
-});
+//   getData();
+//   inputField.value = "";
+//   onloadContainer.innerHTML = "";
+// });
 
-async function getData() {
-  const inputValue = inputField.value;
-  const response = await axios.get(
-    `http://api.tvmaze.com/search/shows?q=${inputValue}`
-  );
+// onPageLoad();
 
-  resultContainer.innerHTML += "";
+// //FUNCTION TO SHOW SOME MOVIES WHEN PAGE LOADS
+// async function onPageLoad() {
+//   const res = await axios.get(`http://api.tvmaze.com/search/shows?q=girls`);
 
-  response.data.forEach((show) => {
-    console.log(show.show);
-    const showName = show.show.name;
+//   onloadContainer.innerHTML = "";
+//   const data = res.data;
 
-    resultContainer.innerHTML += `
-        <div>
-          <p>${showName}</p>
-        </div>
-      `;
-  });
-}
+//   data.forEach((show) => {
+//     console.log(show.show.name);
+
+//     onloadContainer.innerHTML += `
+//     <div>
+//           <div class="uk-card uk-card-default uk-card-body">
+//             <div class="s-grid-1">
+//               <img
+//                 src="${show.show.image.medium}"
+//               />
+
+//               <h2>Name</h2>
+//               <p>
+//                ${show.show.summary}
+//               </p>
+//             </div>
+
+//             <div class="s-grid-2"></div>
+//           </div>
+//         </div>
+//     `;
+//   });
+// }
+
+// //FUNCTION FOR SEARCH
+// async function getData() {
+//   const inputValue = inputField.value;
+//   const response = await axios.get(
+//     `http://api.tvmaze.com/search/shows?q=${inputValue}`
+//   );
+
+//   resultContainer.innerHTML = "";
+
+//   response.data.forEach((show) => {
+//     console.log(show.show);
+//     const showName = show.show.name;
+
+//     resultContainer.innerHTML += `
+//         <div>
+//           <p>${showName}</p>
+//         </div>
+//       `;
+//   });
+// }

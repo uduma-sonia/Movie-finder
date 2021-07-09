992ba27180021908c6f06e5dc8c465a02933ae94
Sonia Uduma <sohnyauduma@gmail.com>
2021-07-09 14:16:00 +0100

    testing API

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// http://api.tvmaze.com/search/shows?q=girls
+const form = document.querySelector("#show-search");
+const inputField = document.querySelector("#search-field");
+const resultContainer = document.querySelector(".result-container");
+const searchBtn = document.querySelector(".search-btn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  getData();
+  inputField.value = "";
+});
+
+async function getData() {
+  const inputValue = inputField.value;
+  const response = await axios.get(
+    `http://api.tvmaze.com/search/shows?q=${inputValue}`
+  );
+
+  resultContainer.innerHTML += "";
+
+  response.data.forEach((show) => {
+    console.log(show.show);
+    const showName = show.show.name;
+
+    resultContainer.innerHTML += `
+        <div>
+          <p>${showName}</p>
+        </div>
+      `;
+  });
+}
